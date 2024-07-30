72a80083ab0a7accfb5df66f624a029986d94e7d
Ranjithkumar3005 <srk30052004@gmail.com>
2024-07-30 22:53:42 +0530

    bulb

---
+++ leetcode/bulb_switcher.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(n**0.5)
+        
+        
+        
+
+s=Solution()
+s.bulbSwitch(n = 3)
