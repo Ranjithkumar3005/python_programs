7840ca7b240db9deee686230fd65fd6d39b7ece6
Ranjithkumar3005 <srk30052004@gmail.com>
2024-07-30 13:38:11 +0530

    min_del

--- leetcode/min_del.py
+++ leetcode/min_del.py
@@ -4,6 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        n = len(s)
+        f = [0] * (n + 1)
+        b = 0
+        for i, c in enumerate(s, 1):
+            print(f)
+            if c == 'b':
+                f[i] = f[i - 1]
+                b += 1
+            else:
+                f[i] = min(f[i - 1] + 1, b)
+        return f[n]
+            
+            
         
         
-        
+s=Solution()
+print(s.minimumDeletions(s = "bbaaaaabbb"))
