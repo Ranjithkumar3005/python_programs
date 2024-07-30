79504802ff6eda0ea704588e3e91adc974ff3816
Ranjithkumar3005 <srk30052004@gmail.com>
2024-07-30 23:26:01 +0530

    verify

---
+++ leetcode/verify_preorder.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+        
+        p = preorder.split(',')
+        
+       
+        slot = 1
+        for node in p:  
+            if slot == 0:
+                return False    
+            if node == '#':
+                slot -= 1
+            else:
+                slot += 1
+        
+        return slot==0
+
+s=Solution()
+s.isValidSerialization(preorder = "9,#,#,1")
