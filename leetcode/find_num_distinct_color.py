550317f830609833dc141301b2a24730c3f14e6a
Ranjithkumar3005 <srk30052004@gmail.com>
2024-08-01 16:55:00 +0530

    FIND

--- leetcode/find_num_distinct_color.py
+++ leetcode/find_num_distinct_color.py
@@ -5,16 +5,42 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
-        h={}
-        d=[]
-        h1={}
-        for i in range(0,len(queries)):
-            h1[queries[i][1]]=0
-            if queries[i][1] not in h1:
-             h[queries[i][0]]=queries[i][1]
-            d.append(len(h))
-        print(d)
-        
+        key_to_value = {}  # Maps each key to its current associated value
+        value_count = {}   # Counts how many keys are associated with each value
+        result = []        # Stores the count of unique values used by keys
 
-s=Solution()
-s.queryResults(limit = 4, queries = [[0,1],[1,2],[2,2],[3,4],[4,5]])
+        unique_value_count = 0  # Tracks the count of unique values
+
+        for a, b in queries:
+            # If the key `a` has a previous association
+            if a in key_to_value:
+                prev_value = key_to_value[a]
+                if prev_value == b:
+                    # If the value remains the same, append current unique value count
+                    result.append(unique_value_count)
+                    continue
+
+                # Decrease count for the old value
+                value_count[prev_value] -= 1
+                if value_count[prev_value] == 0:
+                    unique_value_count -= 1
+
+            # Set the new association
+            key_to_value[a] = b
+
+            # Increase count for the new value
+            if b in value_count:
+                value_count[b] += 1
+            else:
+                value_count[b] = 1
+
+            if value_count[b] == 1:
+                unique_value_count += 1
+
+            # Append the current count of unique values
+            result.append(unique_value_count)
+
+        return result
+
+s = Solution()
+print(s.queryResults(limit=1, queries=[[0, 1], [0, 4], [1, 2], [1, 5], [1, 4]]))  # Output: [1, 1, 2, 2, 1]
