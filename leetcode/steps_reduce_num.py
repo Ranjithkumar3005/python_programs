b647404f1277d716323536a91875de39aa70048c
Ranjithkumar3005 <srk30052004@gmail.com>
2024-08-19 10:44:01 +0530

    steps

---
+++ leetcode/steps_reduce_num.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        c=0
+        while num!=0:
+            if num%2==0:
+                num/=2
+            else:
+                num-=1
+            c+=1
+        print(c)
+        
+
+s=Solution()
+s.numberOfSteps(num = 14)
