d97bd17580157e96be8a9b95cd12378607b9620b
Ranjithkumar3005 <srk30052004@gmail.com>
2024-10-13 02:51:00 +0530

    n

--- leetcode/longest_harnonies_sub.py
+++ leetcode/longest_harnonies_sub.py
@@ -4,13 +4,14 @@ from collections import Counter
 class Solution(object):
     def findLHS(self, nums):
         """
+        
 
         :type nums: List[int]
         :rtype: int
         """
         freq = Counter(nums)
         max_length = 0
-
+ #
         for key in freq:
             if key + 1 in freq:
                 max_length = max(max_length, freq[key] + freq[key + 1])
