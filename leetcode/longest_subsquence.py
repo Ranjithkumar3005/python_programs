add13906b1e831ad2b4c3a5c76d432d177d8d434
Ranjithkumar3005 <srk30052004@gmail.com>
2024-10-29 10:37:48 +0530

    d

---
+++ leetcode/longest_subsquence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestSubsequence(self, arr, difference):
+        """
+        :type arr: List[int]
+        :type difference: int
+        :rtype: int
+        """
+        h = {}
+        maxlength = 0
+        for i in arr:
+            val = i - difference
+            if val in h:
+                h[i] = h[val] + 1
+            else:
+                h[i] = 1
+            maxlength = max(maxlength, h[i])
+        print(maxlength)
+
+
+s = Solution()
+s.longestSubsequence(arr=[1, 5, 7, 8, 5, 3, 4, 2, 1], difference=-2)
