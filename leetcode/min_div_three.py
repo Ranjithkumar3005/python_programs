9ef47638243780934457e44b15b3d426b29bc9f8
Ranjithkumar3005 <srk30052004@gmail.com>
2024-11-03 16:07:17 +0530

    dum

--- leetcode/min_div_three.py
+++ leetcode/min_div_three.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        c=0
-        
-        for i in range(0,len(nums)):
-            if (nums[i]-1)%3==0 or (nums[i]+1)%3==0:
-                c+=1
-        print(c)
-        
-        
+        c = 0
 
-s=Solution()
-s.minimumOperations(nums = [1,2,3,4])
+        for i in range(0, len(nums)):
+            if (nums[i] - 1) % 3 == 0 or (nums[i] + 1) % 3 == 0:
+                c += 1
+        print(c)
+
+
+s = Solution()
+s.minimumOperations(nums=[1, 2, 3, 4])
