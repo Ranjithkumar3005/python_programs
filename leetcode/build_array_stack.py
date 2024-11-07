cbb9b9ce19148bf18927583df55da0a038827e79
Ranjithkumar3005 <srk30052004@gmail.com>
2024-11-07 15:11:17 +0530

    dum

--- leetcode/build_array_stack.py
+++ leetcode/build_array_stack.py
@@ -5,17 +5,20 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        stac=[]
-        c=0
-        for i in range(1,n+1):
-            if target[c]==i:
-                c+=1
+
+        stac = []
+        c = 0
+        for i in range(1, n + 1):
+            if target[c] == i:
+                c += 1
                 stac.append("Push")
-                if c==len(target):
+                if c == len(target):
                     break
             else:
                 stac.append("Push")
                 stac.append("Pop")
         print(stac)
-s=Solution()
-s.buildArray(target = [1,2], n = 4)
+
+
+s = Solution()
+s.buildArray(target=[1, 2], n=4)
