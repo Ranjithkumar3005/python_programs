467c381888fe791520dfd45bc1a3ab4fcd77f197
Ranjithkumar3005 <srk30052004@gmail.com>
2024-12-25 13:03:20 +0530

    25_12

--- leetcode/max-binary.py
+++ leetcode/max-binary.py
@@ -1,25 +1,26 @@
-from collections import deque
+from collections import *
 from typing import Optional, List
 
-# Definition for a binary tree node.
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []  # Handle empty tree
-        
+
         arr = []
         que = deque([root])
-        
+
         while que:
-            dum = float('-inf')  # Initialize to negative infinity
+            dum = float("-inf")  # Initialize to negative infinity
             len1 = len(que)
-            
+
             for i in range(len1):
                 val = que.popleft()
                 dum = max(dum, val.val)
@@ -27,7 +28,7 @@ class Solution:
                     que.append(val.left)
                 if val.right:
                     que.append(val.right)
-            
+
             arr.append(dum)
-        
+
         return arr
